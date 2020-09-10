9f0b5cd4f206b85e60bafa2ba4bcd4ec27984927
Gemuele Aludino <g.aludino@gmail.com>
2020-09-10 01:43:06 -0400

    Change to comment style for documentation (lfk87)

--- keyboards/lfkeyboards/lfk87/keymaps/galudino/config.h
+++ keyboards/lfkeyboards/lfk87/keymaps/galudino/config.h
@@ -1,40 +1,40 @@
-/**
- *  @file       config.h
- *  @brief      Header file for lfk87 layout (keymappings)
- *
- *  @author     Gemuele Aludino
- *  @date       20 Jul 2020
- *  @copyright  Copyright © 2020 Gemuele Aludino
- */
-/**
- *  Copyright © 2020 Gemuele Aludino
- *
- *  Permission is hereby granted, free of charge, to any person obtaining
- *  a copy of this software and associated documentation files (the "Software"),
- *  to deal in the Software without restriction, including without limitation
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *  and/or sell copies of the Software, and to permit persons to whom the
- *  Software is furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included
- *  in all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
- *  THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/*!
+    \file       config.h
+    \brief      Header file for lfk87 layout (keymappings)
+
+    \author     Gemuele Aludino
+    \date       20 Jul 2020
+    \copyright  Copyright © 2020 Gemuele Aludino
+
+
+    Copyright © 2020 Gemuele Aludino
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+    THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 #pragma once
 
 // place overrides here
 
-/*<< Use this directive to enable macOS-related functionality */
+/* Use this directive to enable macOS-related functionality */
 #define ASSUME_APPLE
 
-/*<< USB Device descriptor parameter */
+/* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
@@ -52,6 +52,7 @@
 #define DIODE_DIRECTION COL2ROW
 
 #ifdef LFK_TKL_REV_A
+
 /* RevB Matrix config */
     #define MATRIX_ROWS 6
     #define MATRIX_COLS 17
@@ -70,7 +71,8 @@
     #define UNUSED_PINS {B0, B1, B2, B3, B4, B4, B5, B6, B7, C5, C6, D2, E3, E4, E5, E6, E7, \
                          F0, F1, F4, F5, F6, F7}
     #define RGBLED_NUM 24     // Number of LEDs
-#endif
+
+#endif /* LFK_TKL_REV_A */
 
 #define AUDIO_VOICES
 #define C6_AUDIO
@@ -96,6 +98,7 @@
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
+
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
--- keyboards/lfkeyboards/lfk87/keymaps/galudino/keymap.c
+++ keyboards/lfkeyboards/lfk87/keymaps/galudino/keymap.c
@@ -1,72 +1,82 @@
-/**
- *  @file       keymap.c
- *  @brief      Source file for lfk87 layout (keymappings)
- *
- *  @author     Gemuele Aludino
- *  @date       20 Jul 2020
- *  @copyright  Copyright © 2020 Gemuele Aludino
- */
-/**
- *  Copyright © 2020 Gemuele Aludino
- *
- *  Permission is hereby granted, free of charge, to any person obtaining
- *  a copy of this software and associated documentation files (the "Software"),
- *  to deal in the Software without restriction, including without limitation
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *  and/or sell copies of the Software, and to permit persons to whom the
- *  Software is furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included
- *  in all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
- *  THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+/*!
+    \file       keymap.c
+    \brief      Source file for lfk87 layout (keymappings)
+
+    \author     Gemuele Aludino
+    \date       20 Jul 2020
+    \copyright  Copyright © 2020 Gemuele Aludino
+
+
+    Copyright © 2020 Gemuele Aludino
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+    THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include QMK_KEYBOARD_H
 
-/**
- *  @enum   keymap_layout   Members are used by TG(layer) macro (to toggle a layer on or off)
- *                          to switch between different layers
+/*!
+    \enum   keymap_layout   Members are used by TG(layer) macro (to toggle a layer on or off)
+                            to switch between different layers
  */
 enum keymap_layout {
-    KM_MACOS = 0,     // matches MF68 layout, macOS-centric layout
-    KM_WIN,           // Windows-centric layout
-    KM_FUNC,          // 0x08, function keymap, activated by holding Fn key
-    KM_SETTINGS,      // 0x10, keymap to configure keyboard hardware/firmware settings
+    KM_MACOS = 0,     //<! matches MF68 layout, macOS-centric layout
+    KM_WIN,           //<! Windows-centric layout
+    KM_FUNC,          //<! 0x08, function keymap, activated by holding Fn key
+    KM_SETTINGS,      //<! 0x10, keymap to configure keyboard hardware/firmware settings
 };
 
-// Colors of the layer indicator LED
-// This list needs to define layer 0xFFFFFFFF, it is the end of the list, and the unknown layer
+/*!
+    Colors of the layer indicator LED
+    This list needs to define layer 0xFFFFFFFF,
+    it is the end of the list, and the unknown layer
+
+    from rgblight_list.h
+    RGB_GREEN   0x00, 0xFF, 0x00
+    RGB_BLUE    0x00, 0x00, 0xFF
+    RGB_MAGENTA 0xFF, 0x00, 0xFF
+    RGB_WHITE   0xFF, 0xFF, 0xFF
+*/
 const Layer_Info layer_info[] = {
     // Layer     Mask         Red   Green Blue
-    {0x00000000, 0xFFFFFFFF, {0x00, 0xFF, 0x00}}, // base layers - green
-    {0x00000002, 0xFFFFFFFE, {0x00, 0x00, 0xFF}}, // function layer - blue
-    {0x00000004, 0xFFFFFFFC, {0xFF, 0x00, 0xFF}}, // settings layer - magenta
-    {0xFFFFFFFF, 0xFFFFFFFF, {0xFF, 0xFF, 0xFF}}, // unknown layer - REQUIRED - white
+    {0x00000000, 0xFFFFFFFF, {0x00, 0xFF, 0x00}},    // base layers - green
+    {0x00000002, 0xFFFFFFFE, {0x00, 0x00, 0xFF}},    // function layer - blue
+    {0x00000004, 0xFFFFFFFC, {0xFF, 0x00, 0xFF}},    // settings layer - magenta
+    {0xFFFFFFFF, 0xFFFFFFFF, {0xFF, 0xFF, 0xFF}},    // unknown layer - REQUIRED - white
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap KM_MACOS: (Base Layer) macOS-centric Layer
-   * ,-----------------------------------------------------------------------------.
-   * |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12|  |Prnt|ScLk|Paus|
-   * |-----------------------------------------------------------|  |--------------|
-   * | ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backsp |  | Ins|Home|PgUp|
-   * |-----------------------------------------------------------|  |--------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|  | Del| End|PgDn|
-   * |-----------------------------------------------------------|  `--------------'
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
-   * |-----------------------------------------------------------|        ,----.
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |        | Up |
-   * |-----------------------------------------------------------|   ,-------------.
-   * |Ctrl |Alt | GUI |         Space           | GUI |Func| Ctrl|   |Lft | Dn |Rig|
-   * `-----------------------------------------------------------'   `-------------'
-   */
+  /*! Keymap KM_MACOS: (Base Layer) macOS-centric Layer
+     ,-----------------------------------------------------------------------------.
+     |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12|  |Prnt|ScLk|Paus|
+     |-----------------------------------------------------------|  |--------------|
+     | ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backsp |  | Ins|Home|PgUp|
+     |-----------------------------------------------------------|  |--------------|
+     |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|  | Del| End|PgDn|
+     |-----------------------------------------------------------|  `--------------'
+     |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+     |-----------------------------------------------------------|        ,----.
+     |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |        | Up |
+     |-----------------------------------------------------------|   ,-------------.
+     |Ctrl |Alt | GUI |         Space           | GUI |Func| Ctrl|   |Lft | Dn |Rig|
+     `-----------------------------------------------------------'   `-------------'
+  */
   [KM_MACOS] = LAYOUT_tkl_ansi(
     KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,       KC_PSCR, KC_SLCK, KC_PAUS, \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,      KC_INS,  KC_HOME, KC_PGUP, \
@@ -76,21 +86,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_NO,   KC_RGUI, MO(KM_FUNC),  KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 
-  /* Keymap KM_WIN: (Secondary Layer) Windows-centric Layer
-   * ,-----------------------------------------------------------------------------.
-   * |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12|  |Prnt|ScLk|Paus|
-   * |-----------------------------------------------------------|  |--------------|
-   * | ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backsp |  | Ins|Home|PgUp|
-   * |-----------------------------------------------------------|  |--------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|  | Del| End|PgDn|
-   * |-----------------------------------------------------------|  `--------------'
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
-   * |-----------------------------------------------------------|        ,----.
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |        | Up |
-   * |-----------------------------------------------------------|   ,-------------.
-   * |Ctrl |GUI  |Alt  |        Space           | Alt |Func| Ctrl|   |Lft | Dn |Rig|
-   * `-----------------------------------------------------------'   `-------------'
-   */
+  /*! Keymap KM_WIN: (Secondary Layer) Windows-centric Layer
+     ,-----------------------------------------------------------------------------.
+     |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12|  |Prnt|ScLk|Paus|
+     |-----------------------------------------------------------|  |--------------|
+     | ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backsp |  | Ins|Home|PgUp|
+     |-----------------------------------------------------------|  |--------------|
+     |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|  | Del| End|PgDn|
+     |-----------------------------------------------------------|  `--------------'
+     |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+     |-----------------------------------------------------------|        ,----.
+     |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |        | Up |
+     |-----------------------------------------------------------|   ,-------------.
+     |Ctrl |GUI  |Alt  |        Space           | Alt |Func| Ctrl|   |Lft | Dn |Rig|
+     `-----------------------------------------------------------'   `-------------'
+  */
   [KM_WIN] = LAYOUT_tkl_ansi(
     KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,       KC_PSCR, KC_SLCK, KC_PAUS, \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,      KC_INS,  KC_HOME, KC_PGUP, \
@@ -100,21 +110,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_NO, KC_RALT,MO(KM_FUNC), KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 
-  /* Keymap FUNCTION: Function Layer
-   * ,-------------------------------------------------------------.  ,--------------.
-   * |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12  |  |Prnt|ScLk|Paus|
-   * |-------------------------------------------------------------|  |--------------|
-   * | ` |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Delete   |  |    |    |    |
-   * |-------------------------------------------------------------|  |--------------|
-   * |Tab  |   |PgU|   |   |   |   |   | Up|   |   |   |   |       |  |    |    |    |
-   * |-------------------------------------------------------------|  `--------------'
-   * |Control|Hme|PgD|End|   |   |   |Lft|Dwn|Rgt|   |   |TG(WIN)  |
-   * |-------------------------------------------------------------|       ,----.
-   * |Shift   |   |Del|   |   |   |   |Mute|V- |V+ |  |TG(SETTINGS)|       | Up |
-   * |-------------------------------------------------------------'   ,-------------.
-   * |Func|Win |Alt |        PgD                  | Alt |Ctrl| Func|   |Lft| Dn |Rig |
-   * `-------------------------------------------------------------'   `-------------'
-   */
+  /*! Keymap KM_FUNC: Function Layer
+     ,-------------------------------------------------------------.  ,--------------.
+     |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12  |  |Prnt|ScLk|Paus|
+     |-------------------------------------------------------------|  |--------------|
+     | ` |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Delete   |  |    |    |    |
+     |-------------------------------------------------------------|  |--------------|
+     |Tab  |   |PgU|   |   |   |   |   | Up|   |   |   |   |       |  |    |    |    |
+     |-------------------------------------------------------------|  `--------------'
+     |Control|Hme|PgD|End|   |   |   |Lft|Dwn|Rgt|   |   |TG(WIN)  |
+     |-------------------------------------------------------------|       ,----.
+     |Shift   |   |Del|   |   |   |   |Mute|V- |V+ |  |TG(SETTINGS)|       | Up |
+     |-------------------------------------------------------------'   ,-------------.
+     |Func|Win |Alt |        PgD                  | Alt |Ctrl| Func|   |Lft| Dn |Rig |
+     `-------------------------------------------------------------'   `-------------'
+  */
   [KM_FUNC] = LAYOUT_tkl_ansi(
     KC_ESC,           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_PSCR, KC_SLCK, KC_PAUS, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,            _______, _______, _______, \
@@ -124,20 +134,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,                            KC_PGDN,                            _______, _______, _______, _______,           _______, _______, _______  \
   ),
 
-  /* Keymap KM_SETTINGS: Settings Layer
-   * ,------------------------------------------------------------.  ,-------------.
-   * |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12 |  |Prnt|ScLk|Paus|
-   * |------------------------------------------------------------|  |--------------|
-   * |FN0 |   |   |   |   |   |   |   |   |  |   |BL-|BL+|BL Togl |  |RGB Tog |Val+|
-   * |------------------------------------------------------------|  |-------------|
-   * |MuMode|   |   |   |   |   |   |   |   |   |   |   |  |LEDTst|  |RGB Mode|Val-|
-   * |------------------------------------------------------------|  `-------------'
-   * |AudTgl |Hz+|MS+|   |   |   |   |   |   |   |   |   |  RST   |
-   * |------------------------------------------------------------|       ,----.
-   * |ClickTgl|Hz-|MS-|   |   |   |   |   |   |   |   |Layer Clr  |       |Hue+|
-   * |------------------------------------------------------------------------------.
-   * |    |    |    |                        |    |    |    |     |  |Sat-|Hue-|Sat+|
-   * `------------------------------------------------------------------------------'
+  /*! Keymap KM_SETTINGS: Settings Layer
+     ,------------------------------------------------------------.  ,-------------.
+     |Esc  |f1| f2| f3| f4|  | f5| f6| f7| f8|   | f9|f10|f11|f12 |  |Prnt|ScLk|Paus|
+     |------------------------------------------------------------|  |--------------|
+     |FN0 |   |   |   |   |   |   |   |   |  |   |BL-|BL+|BL Togl |  |RGB Tog |Val+|
+     |------------------------------------------------------------|  |-------------|
+     |MuMode|   |   |   |   |   |   |   |   |   |   |   |  |LEDTst|  |RGB Mode|Val-|
+     |------------------------------------------------------------|  `-------------'
+     |AudTgl |Hz+|MS+|   |   |   |   |   |   |   |   |   |  RST   |
+     |------------------------------------------------------------|       ,----.
+     |ClickTgl|Hz-|MS-|   |   |   |   |   |   |   |   |Layer Clr  |       |Hue+|
+     |------------------------------------------------------------   ,--------------.
+     |    |    |    |                            |    |     |     |  |Sat-|Hue-|Sat+|
+     `------------------------------------------------------------'  `--------------'
    */
   [KM_SETTINGS] = LAYOUT_tkl_ansi(
     KC_FN0,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, BL_DEC,       XXXXXXX, XXXXXXX, XXXXXXX, \
@@ -177,4 +187,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void led_set_user(uint8_t usb_led) {
 
 }
-
