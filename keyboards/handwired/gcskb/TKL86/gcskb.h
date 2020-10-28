41d3c9e7966168563e3ec873357adec336e2ce8d
Gemuele Aludino <g.aludino@gmail.com>
2020-10-27 23:42:37 -0400

    Updated Caps Lock functionality (now Esc) for KM_MACOS and KM_WIN
    The Caps Lock key has been remapped to be Esc --
    but when this key is held, it becomes Fn.
    
    The Fn key in R0 has been removed, and is now RAlt/RGUI
    (dependent if in macOS layer or Windows layer).
    
    The Esc key has been remapped to be Caps Lock.
    When a "held" Esc key is required, invoke the Fn layer
    by holding the "new" Esc key, then press and hold the "old" Esc key.
    The Fn layer has Esc mapped to its "traditional" location.

--- keyboards/handwired/gcskb/TKL86/gcskb.h
+++ keyboards/handwired/gcskb/TKL86/gcskb.h
@@ -96,27 +96,27 @@ enum keymap_layout {
       To switch to LAYER_SETTINGS, press and hold Fn, then it RShift.
 
      ,--------------------------------------------------------------------------.
-     |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12|  |PSc|SLk|Pse|
+     |CPS|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12|  |PSc|SLk|Pse|
      |-----------------------------------------------------------|  |-----------|
      |`  |1  |2  |3  |4  |5  |6  |7  |8  |9  |0  |-  |=  |Backspc|  |Ins|Hme|PgU|
      |-----------------------------------------------------------|  |-----------|
      | Tab |Q  |W  |E  |R  |T  |Y  |U  |I  |O  |P  |[  |]  | \   |  |Del|End|PgD|
      |-----------------------------------------------------------|  `-----------'
-     |CapsLk|A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  | Return |
+     |Esc/Fn|A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  | Return |
      |-----------------------------------------------------------|      ,---.
      | LShift |Z  |X  |C  |V  |B  |N  |M  |,  |.  |/  |  RShift  |      |Up |
      |-----------------------------------------------------------|  ,-----------.
-     |LCtrl|LOp|LCmd |           Space           |RCmd |Fn |RCtrl|  |Lft|Dwn|Rgt|
+     |LCtrl|LOp|LCmd |           Space           |RCmd |ROp|RCtrl|  |Lft|Dwn|Rgt|
      `-----------------------------------------------------------'  `-----------'
    */
 #define LAYER_QWERTY_MAC\
 	KEYMAP_GCSKB_ANSI_7U(\
-		KC_ESC,   KC_F1, KC_F2, KC_F3, KC_F4,   KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9, KC_F10, KC_F11, KC_F12,   KC_PSCR, KC_SLCK, KC_PAUS,\
+		KC_CAPS,   KC_F1, KC_F2, KC_F3, KC_F4,   KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9, KC_F10, KC_F11, KC_F12,   KC_PSCR, KC_SLCK, KC_PAUS,\
 		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,        KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP,\
 		KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,     KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN,\
-		KC_CAPS,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,          KC_ENT,\
+		LT(KM_FUNC, KC_ESC),   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,          KC_ENT,\
 		KC_LSFT,    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,             KC_RSFT,            KC_UP,\
-		KC_LCTL, KC_LALT, KC_LGUI,                  KC_SPC,                   KC_RGUI, MO(KM_FUNC), KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT)\
+		KC_LCTL, KC_LALT, KC_LGUI,                  KC_SPC,                   KC_RGUI, KC_RALT, KC_RCTL,       KC_LEFT, KC_DOWN, KC_RGHT)\
 
   /*!
       Alternate layer for gcskb_TKL86
@@ -132,27 +132,27 @@ enum keymap_layout {
       To switch to LAYER_SETTINGS, press and hold Fn, then it RShift.
 
      ,--------------------------------------------------------------------------.
-     |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12|  |PSc|SLk|Pse|
+     |CAPS|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12|  |PSc|SLk|Pse|
      |-----------------------------------------------------------|  |-----------|
      |`  |1  |2  |3  |4  |5  |6  |7  |8  |9  |0  |-  |=  |Backspc|  |Ins|Hme|PgU|
      |-----------------------------------------------------------|  |-----------|
      | Tab |Q  |W  |E  |R  |T  |Y  |U  |I  |O  |P  |[  |]  | \   |  |Del|End|PgD|
      |-----------------------------------------------------------|  `-----------'
-     |CapsLk|A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  | Return |
+     |Esc/Fn|A  |S  |D  |F  |G  |H  |J  |K  |L  |;  |'  | Return |
      |-----------------------------------------------------------|      ,---.
      | LShift |Z  |X  |C  |V  |B  |N  |M  |,  |.  |/  |  RShift  |      |Up |
      |-----------------------------------------------------------|  ,-----------.
-     |LCtrl|LWn|LAlt |           Space           |RAlt |Fn |RCtrl|  |Lft|Dwn|Rgt|
+     |LCtrl|LWn|LAlt |           Space           |RAlt |RWn|RCtrl|  |Lft|Dwn|Rgt|
      `-----------------------------------------------------------'  `-----------'
    */
 #define LAYER_QWERTY_WIN\
 	KEYMAP_GCSKB_ANSI_7U(\
-		KC_ESC,  KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,   KC_PSCR, KC_SLCK, KC_PAUS,\
+		KC_CAPS,  KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,   KC_PSCR, KC_SLCK, KC_PAUS,\
 		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,      KC_BSPC,  KC_INS,  KC_HOME, KC_PGUP,\
 		KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,    KC_BSLS,  KC_DEL,  KC_END,  KC_PGDN,\
-		KC_CAPS,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,        KC_ENT,\
+		LT(KM_FUNC, KC_ESC),   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,        KC_ENT,\
 		KC_LSFT,    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,           KC_RSFT,           KC_UP,\
-		KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                KC_RALT, MO(KM_FUNC), KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT)\
+		KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                KC_RALT, KC_RGUI, KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT)\
 
   /*!
       Function layer for gcskb_TKL86
@@ -174,11 +174,11 @@ enum keymap_layout {
      |-----------------------------------------------------------|  |-----------|
      | Tab |Q  |PgU|E  |R  |T  |Y  |U  |Up |O  |P  |[  |]  | \   |  |Del|End|PgD|
      |-----------------------------------------------------------|  `-----------'
-     |LCtrl|Hme|PgD|End|F  |G  |H  |Lft|Dwn|Rgt|;  |'  |TG_MACWIN|
+     |     |Hme|PgD|End|F  |G  |H  |Lft|Dwn|Rgt|;  |'  |TG_MACWIN|
      |-----------------------------------------------------------|      ,---.
      | LShift |Z  |Del|C  |V  |B  |N  |Mte|V- |V+ |/  |TG_SETTNGS|      |Up |
      |-----------------------------------------------------------|  ,-----------.
-     |CpsLk|Fn |LCmd |            PgD            |RCmd |Fn |RCtrl|  |Lft|Dwn|Rgt|
+     |LCtrl|GA |LCmd |            PgD            |RCmd |GA |RCtrl|  |Lft|Dwn|Rgt|
      `-----------------------------------------------------------'  `-----------'
 
       While in the macOS layer or Windows layer, pressing and holding the Fn key
@@ -192,12 +192,12 @@ enum keymap_layout {
    */
 #define LAYER_FUNCTION\
 	KEYMAP_GCSKB_ANSI_7U(\
-		KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,\
+		KC_ESC,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,\
 		KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,                          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,\
 		KC_TRNS,     KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,\
-		KC_LCTL,         KC_HOME, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS,    TG(KM_WIN),\
+		KC_TRNS,         KC_HOME, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS,    TG(KM_WIN),\
 		KC_TRNS,             KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS,          TG(KM_SETTINGS),         KC_TRNS,\
-		KC_CAPS, KC_TRNS, KC_TRNS,                             TG(KM_FUNC),                                        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS)\
+		KC_TRNS, KC_TRNS, KC_TRNS,                             KC_PGDN,                                        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS)\
 
   /*!
       Settings layer for gcskb_TKL86
@@ -223,7 +223,7 @@ enum keymap_layout {
    */
 #define LAYER_SETTINGS\
 	KEYMAP_GCSKB_ANSI_7U(\
-		TO(KM_MACOS),        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,\
+		KC_ESC,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,\
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,\
 		KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,\
 		KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      RESET,\
