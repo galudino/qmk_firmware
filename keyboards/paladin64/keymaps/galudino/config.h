8b1d9119cf7d4f045bdc4672b487c456d021e1c7
Gemuele Aludino <g.aludino@gmail.com>
2021-11-26 18:39:09 -0500

    Add galudino layout to paladin64

---
+++ keyboards/paladin64/keymaps/galudino/config.h
@@ -0,0 +1,32 @@
+/*!
+    \file       config.h
+    \brief      Header file for qmk firmware (firmware directives)
+
+    \author     Gemuele Aludino
+    \date       26 Nov 2021
+    \copyright  Copyright (c) 2021 Gemuele Aludino
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define ASSUME_APPLE
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    inachie
+#define PRODUCT         paladin64
+
+#ifdef ASSUME_APPLE
+# undef VENDOR_ID
+# undef PRODUCT_ID
+# define VENDOR_ID          0x05ac  //<! (Apple Inc.)
+# define PRODUCT_ID         0x024f  //<! (Apple Pro Keyboard)
+# define APPLE_FN_ENABLE            //<! Enables keycode KC_APPLE_FN aka KC_APFN
+#endif /* ASSUME_APPLE */
+
+#endif /* CONFIG_H */
