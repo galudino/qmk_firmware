03b10a6b709f8994412fc800e346405a0d4a0142
Gemuele Aludino <g.aludino@gmail.com>
2021-11-26 18:38:33 -0500

    Add KC_APFN on KC_LGUI key for KM_FUNC layer

--- keyboards/lfkeyboards/lfk87/keymaps/galudino/keymap.c
+++ keyboards/lfkeyboards/lfk87/keymaps/galudino/keymap.c
@@ -88,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24,                         _______, _______, _______,   \
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,     _______,                         KC_INS,  _______, _______,   \
     _______,    _______, KC_UP, _______, _______, _______, _______, KC_INS, KC_HOME, KC_PGUP, KC_PSCR, KC_SLCK, KC_PAUS,  _______,      _______,  _______,  _______, \
-    _______,        KC_LEFT, KC_DOWN, KC_RIGHT, KC_APFN, _______, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT, _______, _______,    _______,                                   \
+    _______,        KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT, _______, _______,    _______,                                   \
     _______,            _______, _______, _______, _______, _______, KC_DEL, KC_END, KC_PGDN, _______,  _______, _______,                        _______,            \
-    KC_CAPS, _______, _______,                            MO(KM_SETTINGS),                      _______, _______, KC_APP, _______,      _______, _______, _______    \
+    KC_CAPS, _______, KC_APFN,                            MO(KM_SETTINGS),                      _______, _______, KC_APP, _______,      _______, _______, _______    \
     ),
 
   [KM_SETTINGS] = LAYOUT_tkl_ansi(
