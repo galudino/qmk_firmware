01b171e5ca693df644ed6cf8f41f6ce21aaf0694
Gemuele Aludino <g.aludino@gmail.com>
2021-12-19 14:43:20 -0500

    Update paladin64 keymap and config

--- keyboards/paladin64/keymaps/galudino/keymap.c
+++ keyboards/paladin64/keymaps/galudino/keymap.c
@@ -35,14 +35,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  	KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         LT(KM_FUNC, KC_ESC), KC_A,    KC_S,   KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, 	KC_NUBS,   KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, LT(KM_FUNC, KC_DEL),
-        KC_LCTL, 	KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, KC_RALT,  KC_APP,  KC_RCTL),
+        KC_LCTL, 	KC_LALT, KC_LGUI, KC_SPC, XXXXXXX, KC_RGUI,  KC_RALT,  KC_RCTL),
 
     [KM_FUNC] = LAYOUT_all(
         KC_ESC,   KC_F1,    KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11, KC_F12,  KC_TRNS,  KC_TRNS,
         KC_TRNS,  	KC_TRNS,    KC_UP,   KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_INS,    KC_HOME,    KC_PGUP,    KC_PSCR,    KC_SLCK, KC_PAUS, KC_TRNS,
         KC_TRNS, KC_LEFT,    KC_DOWN,   KC_RGHT,    KC_TRNS,    KC_TRNS,   KC_LEFT,   KC_UP,    KC_DOWN,    KC_RGHT,    KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, 	KC_NUBS,   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_DEL,    KC_END,    KC_PGDN, KC_TRNS,  KC_TRNS, KC_APFN, KC_TRNS,
-        KC_CAPS, 	KC_TRNS, KC_APFN, MO(KM_SETTINGS), KC_TRNS, XXXXXXX,   KC_RALT,  KC_TRNS),
+        KC_CAPS, 	KC_TRNS, KC_APFN, MO(KM_SETTINGS), XXXXXXX, KC_TRNS,  KC_APP,  KC_TRNS),
 
     [KM_SETTINGS] = LAYOUT_all(
         GE_SWAP,   KC_MUTE,    KC_VOLD,   KC_VOLU,    KC_MPRV,    KC_MPLY,   KC_MNXT,   KC_MSTP,    KC_BRIU,    KC_BRID,    XXXXXXX,    KC_MYCM, KC_WAKE,  XXXXXXX,  KC_PWR,
