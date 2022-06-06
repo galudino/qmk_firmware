d2fa2b002646883167d091ccc693471fbea78c3b
Gemuele Aludino <g.aludino@gmail.com>
2022-06-05 21:29:30 -0400

    Add layout `galudino-keytrak` for `hineybush/h88`

---
+++ keyboards/hineybush/h88/keymaps/galudino-keytrak/config.h
@@ -0,0 +1,24 @@
+/*!
+    \file       config.h
+    \brief      Header file for qmk firmware (firmware directives)
+
+    \author     Gemuele Aludino
+    \date       16 Oct 2021
+    \copyright  Copyright (c) 2021 Gemuele Aludino
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define ASSUME_APPLE
+
+#ifdef ASSUME_APPLE
+# undef VENDOR_ID
+# undef PRODUCT_ID
+# define VENDOR_ID  0x05ac  //<! (Apple Inc.)
+# define PRODUCT_ID 0x024f  //<! Apple Pro keyboard
+# define APPLE_FN_ENABLE    //<! Enables keycode KC_APPLE_FN aka KC_APFN
+
+#endif /* ASSUME_APPLE */
+
+#endif /* CONFIG_H */
